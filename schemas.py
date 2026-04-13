c25a9bf9123093b732a183dfa80591aa955caeda
zk222ac001 <157967263+zk222ac001@users.noreply.github.com>
2026-04-14 00:12:31 +0200

    Enhance schemas.py with model documentation
    Added comments to explain the purpose of BookCreate and BookResponse models.

--- schemas.py
+++ schemas.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
+# BaseModel → Grundklassen i Pydantic
+# Bruges til: --> Data validering --> API request/response modeller (fx i FastAPI)
 
+# BookCreate (input model) ......................................
+# Denne klasse bruges når du opretter en bog (POST request)
 class BookCreate(BaseModel):
     title: str
     author: str
     pages: int
 
+# BookResponse (output model)..............................................
+# Denne bruges når du returnerer data fra API’et
 class BookResponse(BookCreate):
     id: int
 
